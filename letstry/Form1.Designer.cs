dce1fd29de70ef89c77156fe98403a7dea984165
kh4li <kh4li@DESKTOP-40FEHNM>
2024-10-30 23:00:05 +0300

    Add project files.

---
+++ letstry/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace letstry
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Input_Reader = new System.Windows.Forms.TextBox();
+            this.Output = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Input_Reader
+            // 
+            this.Input_Reader.AccessibleDescription = "";
+            this.Input_Reader.AccessibleName = "Input_Reader";
+            this.Input_Reader.Location = new System.Drawing.Point(30, 68);
+            this.Input_Reader.Name = "Input_Reader";
+            this.Input_Reader.Size = new System.Drawing.Size(419, 20);
+            this.Input_Reader.TabIndex = 0;
+            this.Input_Reader.Text = "Enter the number";
+            this.Input_Reader.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Input_Reader_KeyDown);
+            // 
+            // Output
+            // 
+            this.Output.AccessibleName = "";
+            this.Output.AutoSize = true;
+            this.Output.Font = new System.Drawing.Font("Lucida Handwriting", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Output.Location = new System.Drawing.Point(54, 171);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(257, 21);
+            this.Output.TabIndex = 2;
+            this.Output.Tag = "";
+            this.Output.Text = "The Result is shown here.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(419, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter the Number one Million and Above Zero Please";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F);
+            this.label2.Location = new System.Drawing.Point(26, 398);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(559, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "If a number is greater than 1 million it will only read lasts five digits of it";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Input_Reader);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Spelling Giver";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Input_Reader;
+        private System.Windows.Forms.Label Output;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ letstry/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace letstry
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void Input_Reader_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                //Output.Text = Input_Reader.Text; // Display TextBox input in the label
+                Processor processor = new Processor();
+                Output.Text = processor.ProcessInput(Input_Reader.Text);
+                e.SuppressKeyPress = true;   // Optional: suppress the "ding" sound when Enter is pressed
+            }
+        }
+    }
+}
+
+
+public class Processor
+{
+    private long Num = 0;
+    private long temp = 0;
+    private long unit = 0, ten = 0, hundred = 0;
+    private string number;
+    private string spelling;
+    private readonly string[] str = { " ", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+    private readonly string[] str2 = { " ", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+    private readonly string hnd = "Hundred", th = "Thousand";
+
+    // Method to process input and return result
+    public string ProcessInput(string userInput)
+    {
+        // Example operation: Convert to uppercase
+        string result = userInput.ToUpper(); // Replace with your specific logic
+        number = result;
+        if (TypeChecker(number))
+        {
+            Num = long.Parse(number);// Parsing string into long int
+            if (Num==0)
+            {
+                return "Zero";
+            }
+            return LengthChecker();
+        }
+        else
+        {
+            return "Please enter the only Positive Number";
+        }
+        
+        
+       
+        return result;
+    }
+    private bool TypeChecker(string number) /*Checking whether string is valid conatains only numbers, nothing else*/
+    {
+        if (Regex.IsMatch(number, @"^(\+|-)?\d+$"))
+        {
+                return true;
+        }
+        else
+        {
+            Console.WriteLine("Wrong Input (other than numbers) ");
+            return false;
+          //  UserInput();/*need to build this logic again if nedded*/ not needed
+        }
+        
+    }
+    private string LengthChecker()
+    {
+        
+        if (Num < 0) Num *= -1;
+
+        if (Num < 1000) { return BreakNumber(Num); };
+        if (Num % 100000 == 0) { return "Zero"; }
+        if (Num > 999) {
+            long temp = (int)(Num / 1000);
+            temp = (int)(temp % 1000);
+            string temp2=BreakNumber(temp);
+            Console.WriteLine("Thousand ");
+            temp = (int)(Num % 1000);
+            string temp3=BreakNumber(temp);
+            return temp2 + "Thousand " + temp3;
+        }
+        return "";
+    }
+    private string BreakNumber(long number1)
+    {
+        unit = number1 % 10;
+        temp = number1 / 10;
+        ten = temp % 10;
+        temp = number1 / 100;
+        hundred = temp % 10;
+        return Print(unit, ten, hundred, number1);
+    }
+    private string Print(long unit, long ten, long hundred, long number1)
+    {
+        if (number1 == 0)
+        {
+            Console.WriteLine("Zero");
+        }
+        else if (number1 < 20)
+        {
+            spelling = str[number1] + " ";
+        }
+        else if (number1 > 19 && number1 < 100)
+        {
+            spelling = str2[ten] + " " + str[unit] + " ";
+        }
+        else if (number1 > 99 && number1 < 1000)
+        {
+            if (ten < 2)
+            {
+                temp = number1 % 100;
+                spelling = str[hundred] + " " + hnd + " " + str[temp] + " ";
+            }
+            else
+            {
+                spelling = str[hundred] + " " + hnd + " " + str2[ten] + " " + str[unit] + " ";
+            }
+        }
+        return spelling;
+    }
+
+
+}
